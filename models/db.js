c08afd7f15f64b2bb7e1dedc89aa8b3f8962ca6c
Leon liu <lzx_bupt@126.com>
2015-09-06 02:52:31 -0400

    [MODEL] mongodb connection port fix

--- models/db.js
+++ models/db.js
@@ -4,10 +4,13 @@
 
 var settings = require('../settings');
 var Db = require('mongodb').Db;
-var Connection = require('mongodb').Connection;
+var mongo = require("mongodb");
+
+// TODO: Check Connection object usage
+// var Connection = require('mongodb').Connection;
+
 var Server = require('mongodb').Server;
 
-module.exports = new Db(settings.db, new Server(settings.host,
-        Connection.DEFAULT_PORT, {}), {
+module.exports = new Db(settings.db, new Server(settings.host, 27017, {}), {
     safe : true
 });
