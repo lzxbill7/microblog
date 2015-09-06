88239a5f5e3a8e4d07000f0f427a9d31ef23f214
Leon liu <lzx_bupt@126.com>
2015-09-06 01:23:12 -0400

    [REG] Initial registry page

--- routes/index.js
+++ routes/index.js
@@ -30,4 +30,40 @@ router.get("/reg", function(req, res) {
     });
 });
 
+router.post("/reg", function(req, res) {
+    if (req.body['password-repeat'] != req.body['password']) {
+        req.flash('error', '两次输入的口令不一致');
+        return res.redirect('/reg');
+    }
+    console.log(req.body['password'])
+
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+
+    return res.redirect('/reg');
+    // var newUser = new User({
+    // name : req.body.username,
+    // password : password,
+    // });
+    // // 检查用户名是否已经存在
+    // User.get(newUser.name, function(err, user) {
+    // if (user)
+    // err = 'Username already exists.';
+    // if (err) {
+    // req.flash('error', err);
+    // return res.redirect('/reg');
+    // }
+    //
+    // newUser.save(function(err) {
+    // if (err) {
+    // req.flash('error', err);
+    // return res.redirect('/reg');
+    // }
+    // req.session.user = newUser;
+    // req.flash('success', '注册成功');
+    // res.redirect('/');
+    // });
+    // });
+});
+
 module.exports = router;
