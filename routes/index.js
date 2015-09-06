a0f0b77baf23c33062a69b47d9c46e149c4c086a
Leon liu <lzx_bupt@126.com>
2015-09-06 05:25:00 -0400

    [ROUTE] Making post working

--- routes/index.js
+++ routes/index.js
@@ -123,19 +123,19 @@ router.get("/logout", function(req, res) {
 });
 
 // post route
-// router.post("/post", checkLogin);
-// router.post("/post", function(req, res) {
-// var currentUser = req.session.user;
-// var post = new Post(currentUser.name, req.body.post);
-// post.save(function(err) {
-// if (err) {
-// req.flash('error', err);
-// return res.redirect('/');
-// }
-// req.flash('success', 'post successfully');
-// res.redirect('/u/' + currentUser.name);
-// });
-// });
+router.post("/post", checkLogin);
+router.post("/post", function(req, res) {
+    var currentUser = req.session.user;
+    var post = new Post(currentUser.name, req.body.post);
+    post.save(function(err) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        req.flash('success', 'post successfully');
+        res.redirect('/u/' + currentUser.name);
+    });
+});
 
 // user route
 router.get("/u/:user", function(req, res) {
